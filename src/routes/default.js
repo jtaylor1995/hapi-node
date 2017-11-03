a43dc7a7af1b717aa117e9751efda677b9b18152
James Taylor <j.taylor@kainos.com>
2017-11-03 14:41:11 +0000

    Pushing broke for mark

--- src/routes/default.js
+++ src/routes/default.js
@@ -1,5 +1,17 @@
-const get = (errors, field) => { return errors.filter(error => error.field == field )[0].description }
-const isError = (errors, field) => { return errors.filter(error => error.field == field).length == 1}
+const handlebars = require('handlebars')
+// Are there any errors
+const isErrors = (errors) => {
+    return errors.length > 0
+};
+// Is there a specific error
+const isError = (errors, field) => {
+    return errors.filter(error => error.field == field).length == 1
+};
+// Get an error description
+const get = (errors, field) => {
+    return errors.filter(error => error.field == field)[0].description
+};
+
 
 
 module.exports = [{
@@ -18,27 +30,40 @@ module.exports = [{
   method: 'POST',
   path: '/form',
   handler: function (request, reply) {
+
     var errors = []
 
-       if(!request.payload.name)
-            errors.push({description: 'Please provide your name', field: 'name'})
+    if(!request.payload.name)
+      errors.push({description: 'Please provide your name', field: 'name'})
+    if(!request.payload.company)
+      errors.push({description: 'Please provide your company', field: 'company'})
 
-       if(!request.payload.company)
-            errors.push({description: 'Please provide your company', field: 'company'})
+    handlebars.registerHelper("isError", function(errors, field, options) {
+        if(isError(errors, field)) {
+          return options.fn(this);
+        } else {
+          return options.inverse(this)
+        }
+    });
 
-             if(errors.length) {
-                    reply.view('form', {
-                    'data': request.payload,
-                    'areErrors': errors.length > 0,
-                    'nameError': isError(errors, 'name'),
-                    'nameDesc': get(errors, 'name'),
-                    'companyError': isError(errors, 'company'),
-                    'companyDesc': get(errors, 'company'),
-                    'errors': errors})
-                } else {
-                // MAKE THIS GO TO /VIEW
-                    reply.redirect('/')
-                }
+    handlebars.registerHelper("get", function(errors, field) {
+      return get(errors, field)
+    });
+
+   const viewContext = {
+     'data': request.payload,
+     'errors': errors,
+     'isErrors': function () {
+         return isErrors
+     }
+   }
+
+    if(errors.length) {
+       reply.view('form', viewContext)
+    } else {
+       // MAKE THIS GO TO /VIEW
+       reply.redirect('/')
+    }
 
   }
 }
