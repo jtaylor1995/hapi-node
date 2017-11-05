39ad7264bd5afadae094f7ef26293535d2b5d47b
James Taylor <j.taylor@kainos.com>
2017-11-05 10:16:55 +0000

    Error validation added using handlebars

--- src/routes/default.js
+++ src/routes/default.js
@@ -12,17 +12,33 @@ const get = (errors, field) => {
     return errors.filter(error => error.field == field)[0].description
 };
 
+handlebars.registerHelper( "isError", function(errors, field, options) {
+    if(isError(errors, field)) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this)
+    }
+});
 
+handlebars.registerHelper('get', function(errors, field) {
+  return get(errors, field)
+});
 
 module.exports = [{
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
+
+    var errors = []
     console.log('Requested index page')
-    const viewContext = {}
 
-    viewContext.pageTitle = 'Example Page'
-
+    const viewContext = {
+        'pageTitle': 'Example Page',
+        'errors': errors,
+        'isErrors': function () {
+            return isErrors(errors)
+          }
+    }
     reply.view('form', viewContext)
   }
 },
@@ -35,27 +51,18 @@ module.exports = [{
 
     if(!request.payload.name)
       errors.push({description: 'Please provide your name', field: 'name'})
+
     if(!request.payload.company)
       errors.push({description: 'Please provide your company', field: 'company'})
 
-    handlebars.registerHelper("isError", function(errors, field, options) {
-        if(isError(errors, field)) {
-          return options.fn(this);
-        } else {
-          return options.inverse(this)
-        }
-    });
-
-    handlebars.registerHelper("get", function(errors, field) {
-      return get(errors, field)
-    });
 
    const viewContext = {
+     'pageTitle': 'Example Page',
      'data': request.payload,
      'errors': errors,
      'isErrors': function () {
-         return isErrors
-     }
+         return isErrors(errors)
+       }
    }
 
     if(errors.length) {
@@ -66,5 +73,4 @@ module.exports = [{
     }
 
   }
-}
-]
+}];
