673a0c3ba140bc8b01eb319d2fa91e59502d1f70
James Taylor <j.taylor@kainos.com>
2017-11-07 14:45:15 +0000

    Added Chai and Mocha instead of Lab and Code

--- test/endpointTestExample.js
+++ test/endpointTestExample.js
@@ -1,36 +1,35 @@
-const Lab = require('lab')
-const lab = exports.lab = Lab.script()
-const Code = require('code')
 const server = require('../index.js')
-
 var request = require('supertest');
+var Chai = require('Chai')
 
 
-lab.experiment('Basic HTTP Tests', () => {
-  lab.test('GET / (index endpoint test)', (done) => {
+describe('Endpoint', function() {
+
+  it('should return 200 status', function(done) {
+    request(server.listener)
+      .get('/')
+      .expect(200)
+
+    server.stop(done)
+  });
+});
+
+describe('Endpoint inject', function() {
+
+  it('should return 200 status without supertest', function(done) {
+
     var options = {
       method: 'GET',
       url: '/'
     }
-
     // Simulate an http request
     server.inject(options, (response) => {
       //  Expect http response status code to be 200 ('Ok')
-      Code.expect(response.statusCode).to.equal(200)
-
+      Chai.expect(response.statusCode).to.equal(200)
       // Expect some content to be on the returned page
-      Code.expect(response.result).to.contain('Submit Registration')
+      Chai.expect(response.result).to.contain('Submit Registration')
 
       server.stop(done)  // done() callback is required to end the test.
     })
-  })
-})
-
-
-lab.experiment('Basic HTTP Supertest', () => {
-  lab.test('GET / (index page test)', (done) => {
-    request(server.listener)
-      .get('/')
-      .expect(200, done);
-  })
-})
+  });
+});
