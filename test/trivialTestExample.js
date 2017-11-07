000108c53591bb83ca072289ef741ffb48077cb6
James Taylor <j.taylor@kainos.com>
2017-11-07 15:07:55 +0000

    Update trivial tests to use mocha and chai

--- test/trivialTestExample.js
+++ test/trivialTestExample.js
@@ -1,12 +1,13 @@
-// const Lab = require('lab')
-// const lab = exports.lab = Lab.script()
-// const Code = require('code')
-//
-// lab.test('returns true when 1 + 1 equals 2', (done) => {
-//   Code.expect(1 + 1).to.equal(2)
-//
-//   // How to expect the length of some item
-//   Code.expect('something').to.have.length(9)
-//
-//   done()
-// })
+var Chai = require('Chai')
+
+describe('Trivial', function() {
+
+  it('should perform trivial test', function(done) {
+    Chai.expect(1 + 1).to.equal(2)
+
+    // How to expect the length of some item
+    Chai.expect('something').to.have.length(9)
+
+    done()
+  });
+});
